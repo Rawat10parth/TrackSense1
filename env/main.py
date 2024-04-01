be76e44142557c4a54a200eec40e849dd0f9bb87
Parth Rawat <parthpriyanshu10@gmail.com>
2024-04-01 20:45:13 +0530

    Additional changes

--- env/main.py
+++ env/main.py
@@ -20,6 +20,7 @@ class ScreenTimeTracker:
         self.idle_time = 5 * 60  # 5 minutes
         self.activity_detected = False  # Flag to track any activity
         self.last_window_title = None
+        self.last_application = None
 
         # Start the idle checker in a separate thread
         idle_checker = threading.Thread(target=self.check_idle)
@@ -32,7 +33,7 @@ class ScreenTimeTracker:
 
     # Define the idle check function
     def check_idle(self):
-        current_window = None
+        current_application = None
         window_start_time = None
 
         while True:
@@ -51,21 +52,22 @@ class ScreenTimeTracker:
             # Capture active window title
             active_window = gw.getActiveWindow()
             window_title = active_window.title if active_window else ""
+            application = window_title.split(" - ")[-1] if active_window else ""
             print(f"Active window title: {window_title}")
 
             # If the active window changes
-            if window_title != current_window:
+            if window_title != current_application:
                 # Calculate time spent on previous window and add to total_time
                 if window_start_time is not None:
-                    window_elapsed_time = time.time() - window_start_time
-                    self.total_time += window_elapsed_time
-                    hours, rem = divmod(window_elapsed_time, 3600)
+                    application_elapsed_time = time.time() - window_start_time
+                    self.total_time += application_elapsed_time
+                    hours, rem = divmod(application_elapsed_time, 3600)
                     minutes, seconds = divmod(rem, 60)
-                    print(f"Time spent on '{current_window}': {int(hours)}:{int(minutes)}:{int(seconds)}")
-                    update_or_insert_window_time(current_window, window_elapsed_time)
+                    print(f"Time spent on '{current_application}': {int(hours)}:{int(minutes)}:{int(seconds)}")
+                    update_or_insert_window_time(current_application, application_elapsed_time)
 
                 # Update current window and start time
-                current_window = window_title
+                current_application = application
                 window_start_time = time.time()
 
             # Sleep for idle check interval
